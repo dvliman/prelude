c2360cb35736b592ca2230f37e288c8406eb8fd9
David Liman <limanoit@gmail.com>
2021-01-02 18:14:33 -0800

    dvliman: personal custom.el

---
+++ personal/custom.el
@@ -0,0 +1,11 @@
+(setq org-directory "/Users/dv/Library/Mobile Documents/com~apple~CloudDocs/org")
+(setq org-default-notes-files "/Users/dv/Library/Mobile Documents/com~apple~CloudDocs/org/agenda.org")
+(global-set-key (kbd "C-c C-f") 'helm-rg)
+(setq default-frame-alist '((font . "Jetbrains Mono-20")))
+
+(custom-set-variables
+ '(package-selected-packages
+   '(paredit ranger treemacs-projectile adoc-mode go-mode helm-rg helm-projectile helm treemacs treemacs-evil dockerfile-mode projectile-ripgrep clojure-mode yaml-mode web-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text magit lsp-ui json-mode js2-mode ivy-prescient imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-lsp browse-kill-ring anzu ag ace-window cider)))
+(custom-set-faces
+
+ )
