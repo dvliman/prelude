46c2ecb25354ea694e3aab9a49b76f2500ba8df8
David Liman <dliman@aspiration.com>
2021-01-03 00:06:07 -0800

    prelude theme: solarized light

--- personal/custom.el
+++ personal/custom.el
@@ -1,11 +1,16 @@
-(setq org-directory "/Users/dv/Library/Mobile Documents/com~apple~CloudDocs/org")
-(setq org-default-notes-files "/Users/dv/Library/Mobile Documents/com~apple~CloudDocs/org/agenda.org")
 (global-set-key (kbd "C-c C-f") 'helm-rg)
 (setq default-frame-alist '((font . "Jetbrains Mono-20")))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(paredit ranger treemacs-projectile adoc-mode go-mode helm-rg helm-projectile helm treemacs treemacs-evil dockerfile-mode projectile-ripgrep clojure-mode yaml-mode web-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text magit lsp-ui json-mode js2-mode ivy-prescient imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-lsp browse-kill-ring anzu ag ace-window cider)))
+   '(paredit ranger treemacs-projectile adoc-mode go-mode helm-rg helm-projectile helm treemacs treemacs-evil dockerfile-mode projectile-ripgrep clojure-mode yaml-mode web-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text magit lsp-ui json-mode js2-mode ivy-prescient imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-lsp browse-kill-ring anzu ag ace-window cider solarized-themes)))
 (custom-set-faces
-
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
